0232af00dd1e3844cedd879ff3be46d94f1f9813
Courtney Ficker <cficker@pdx.edu>
2022-03-08 03:44:28 -0800

    Got bootstrap menu working, couple of JS functions to control navigation

--- script.js
+++ script.js
@@ -2,4 +2,14 @@ $(document).ready(() => {
     $('#temp').text("Courtney's page");
     
   });
-  
+
+function mark_active(curr) {
+  $(".nav-link").removeClass("active");
+  curr.addClass("active");
+}
+
+$(".nav-link").on('click', function () {
+  console.log($(this))
+  alert($(this).attr("id"));
+  mark_active($(this));
+});
