279b446040ff95bd74300e2ebf3c725678ea3a72
Courtney Ficker <cficker@pdx.edu>
2022-03-08 10:37:47 -0800

    Really blinged out my footer. Also toying with a css spinner but it's not behaving.

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # CS410-webdev-final
 Final Project for CS410 Web Development (Winter 2022)
+
+## Sources/Inspirations
+* Navbar design inspired by examples on the [Bootstrap home page](https://getbootstrap.com/)
+* Footer adapted from examples on [Material Design for Bootstrap](https://mdbootstrap.com/docs/standard/navigation/footer/)
+* Free social media .svg icons from [Iconfinder](https://iconfinder.com)
+* Pure CSS spinner from [Loadion.IO](https://loading.io/css/)
--- script/script.js
+++ script/script.js
@@ -1,18 +1,17 @@
 $(document).ready(() => {
-    $('#temp').text("Courtney's page");
-    
+  $("#loading").addClass(".d-none");
+
+  $(".nav-link").on("click", function () {
+    function mark_active(curr) {
+      // Remove active class from previous link, and add it to the one just clicked
+      $(".active").removeClass("active");
+      curr.addClass("active");
+    }
+
+    mark_active($(this));
+
+    // Need to replace this with something useful (loading content)
+    console.log("Loading content: " + $(this).attr("id"));
+    $("main").load("pages/lorem.html #lorem");
   });
-
-$(".nav-link").on('click', function () {
-  function mark_active(curr) {
-    // Remove active class from previous link, and add it to the one just clicked
-    $(".active").removeClass("active");
-    curr.addClass("active");
-  }
-
-  mark_active($(this));
-
-  // Need to replace this with something useful (loading content)
-  console.log("Loading content: " + $(this).attr("id"));
-  $("main").load("pages/lorem.html #lorem");
 });
