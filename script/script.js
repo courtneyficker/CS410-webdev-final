ac61ee344163b1656f78a3de6a76ee98d76cfa4c
Courtney Ficker <cficker@pdx.edu>
2022-03-08 03:51:38 -0800

    Moved files into directories, prettied JS a bit

---
+++ script/script.js
@@ -0,0 +1,17 @@
+$(document).ready(() => {
+    $('#temp').text("Courtney's page");
+    
+  });
+
+$(".nav-link").on('click', function () {
+  function mark_active(curr) {
+    // Remove active class from previous link, and add it to the one just clicked
+    $(".active").removeClass("active");
+    curr.addClass("active");
+  }
+
+  mark_active($(this));
+
+  // Need to replace this with something useful (loading content)
+  console.log("Loading content: " + $(this).attr("id"));
+});
